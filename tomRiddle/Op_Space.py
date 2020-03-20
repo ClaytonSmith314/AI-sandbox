50189a8bf52e9c5de351ae06808e3fc1093bab2e
ClaytonSmith314 <44728116+ClaytonSmith314@users.noreply.github.com>
2020-03-19 20:08:16 -0500

    start working with asyncio

--- tomRiddle/Op_Space.py
+++ tomRiddle/Op_Space.py
@@ -7,6 +7,11 @@ import Memory
 # here's where other coroutines go
 
 
+async def count_down(n):
+    while n >= 0:
+        print(n)
+        await asyncio.sleep(1)
+        n -= 1
 
 
 
@@ -14,10 +19,14 @@ import Memory
 # main coroutine. only runs once.
 async def main():
     print("entering main function")
-    await asyncio.sleep(1)
+    t = asyncio.create_task(count_down(10))
+    await asyncio.sleep(2.5)
     await memexp.fillrand(10)
     print(Memory.memory)
+    print(len(Memory.memory))
     await asyncio.sleep(5)
+    print("tea time!")
+    await t
     print("exiting main")
     setup.close()
 
