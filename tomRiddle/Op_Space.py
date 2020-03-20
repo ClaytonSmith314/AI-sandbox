bbd51f4c055f1630344a5d9c315e4c28b874b8b1
ClaytonSmith314 <44728116+ClaytonSmith314@users.noreply.github.com>
2020-03-20 00:01:42 -0500

    asyncio experiment

--- tomRiddle/Op_Space.py
+++ tomRiddle/Op_Space.py
@@ -1,32 +1,22 @@
 
 import asyncio
 import setup
+from expiriment import exp_mod
 from expiriment import memexp
 import Memory
 
 # here's where other coroutines go
 
-
-async def count_down(n):
-    while n >= 0:
-        print(n)
-        await asyncio.sleep(1)
-        n -= 1
-
-
+running = {
+}
 
 
 # main coroutine. only runs once.
 async def main():
     print("entering main function")
-    t = asyncio.create_task(count_down(10))
-    await asyncio.sleep(2.5)
-    await memexp.fillrand(10)
-    print(Memory.memory)
-    print(len(Memory.memory))
-    await asyncio.sleep(5)
-    print("tea time!")
-    await t
+    running[asyncio.create_task(exp_mod.exampleCoroutine())] = {}
+    for task in running.keys():
+        await task
     print("exiting main")
     setup.close()
 
--- tomRiddle/expiriment/exp_mod.py
+++ tomRiddle/expiriment/exp_mod.py
@@ -1,4 +1,7 @@
 
+import asyncio
+import Memory
+from expiriment import memexp
 
 mystring = "MU"
 
@@ -7,3 +10,18 @@ def mymethod(append):
     global mystring
     mystring = mystring + append
 
+async def count_down(n):
+    while n >= 0:
+        print(n)
+        await asyncio.sleep(1)
+        n -= 1
+
+async def exampleCoroutine():
+    t = asyncio.create_task(count_down(10))
+    await asyncio.sleep(2.5)
+    await memexp.fillrand(10)
+    print(Memory.memory)
+    print(len(Memory.memory))
+    await asyncio.sleep(5)
+    print("tea time!")
+    await t
